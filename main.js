90ec3549f8f351e940f5dddfb07ad78a327b0a1b
Ayeni Oluwaseun Daniel <103773261+Ayeni173@users.noreply.github.com>
2022-08-09 11:13:39 +0100

    Working on JS

--- main.js
+++ main.js
@@ -10,3 +10,34 @@ seconddiv.addEventListener('click', function () {
 
 // changing the element background
 seconddiv.style.background = "blue" 
+
+// console
+
+// console.log('welcome, Daniel, this works !!!')
+
+// console.log(document)
+
+// alert
+// alert('welcome to my first day with Javascript')
+
+// (innerHtml)
+// document.querySelector(".right-holder").innerHTML = "Supper cool!! it works"
+
+function popsomething() {
+    alert ('welcome, box 1 is clicked')
+}
+
+var rightdiv = document.querySelector('#right-holder')
+console.log(rightdiv)
+
+function changeTO_Orange() {
+    rightdiv.style.background = "#ffa800"
+}
+
+function changeTo_Red() {
+    rightdiv.style.background ='red'
+}
+
+function changeTo_gray() {
+    rightdiv.style.background ='gray'
+}
